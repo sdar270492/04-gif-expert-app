0af42a0781b080d32d63f1dc59e1028664e2b555
Sergio Aguilar <sergioaguilar.ti@outlook.com>
2022-11-24 00:02:24 -0500

    final
    final

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,36 +1,37 @@
 import { useState } from "react";
-import { AddCategory } from "./components/AddCategory";
+import { AddCategory, GifGrid } from "./components";
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+    const [categories, setCategories] = useState(['One Punch']);
 
     const onAddCategory = ( newCategory ) => {
-        setCategories([...categories, 'Valorant']);
 
-        // setCategories(cat => [ newCategory, ...categories]);
+        if ( categories.includes(newCategory) ) return;
+
+        // setCategories([...categories, 'Valorant']);
+
+        setCategories(cat => [ newCategory, ...categories]);
     }
 
 
     return (
         <>
-            {/* titulo */}
             <h1>GifExpertApp</h1>
 
-            {/* Input */}
             <AddCategory 
-                // setCategories={ setCategories }
                 onNewCategory={ onAddCategory }
-                />
-            
-            {/* Listado de Gif */}
-            <ol>
-                {
-                    categories.map( category => {
-                        return <li key={ category }>{ category }</li>
-                    })
-                }
-            </ol>
+            />
+
+            {
+                categories.map( category => (
+                        <GifGrid  
+                            key = { category } 
+                            category = { category }
+                        />
+                    )
+                )
+            }
         </>
     )
 }
